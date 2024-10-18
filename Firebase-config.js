8ff2d0bd155167fe5e6f3b799dfac6a4493e3171
KhairaNajla <khaira.najla@gmail.com>
2024-10-19 00:19:02 +0700

    baru

---
+++ Firebase-config.js
@@ -0,0 +1,13 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyB8AaquaBjIzgydP2wdp-4y473kHHKjOR0",
+    authDomain: "tugas-cc-6411f.firebaseapp.com",
+    databaseURL: "https://tugas-cc-6411f-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "tugas-cc-6411f",
+    storageBucket: "tugas-cc-6411f.appspot.com",
+    messagingSenderId: "672591336363",
+    appId: "1:672591336363:web:923c3c68d68a6d987fb8ee",
+    measurementId: "G-PPE6C8B9Y4"
+  };
+
+const app = firebase.initializeApp(firebaseConfig);
